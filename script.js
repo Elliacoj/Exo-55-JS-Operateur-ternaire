b7d192b17f40aa2f064b115b582596c25a0fca1f
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-11-25 11:21:27 +0100

    Modif

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let paragraphe = document.getElementById('paragraphe');
 let span5 = document.getElementById('span5');
 let span12 = document.getElementById('span12');
 let resultat = document.getElementById('resultat');
+let container = document.getElementById('container');
 
 resultat.innerHTML = (parseInt(span5.innerText) < parseInt(span12.innerText)) ? (parseInt(span5.innerText) + parseInt(span12.innerText)).toString() :
     (parseInt(span5.innerText) - parseInt(span12.innerText)).toString();
@@ -9,3 +10,5 @@ resultat.innerHTML = (parseInt(span5.innerText) < parseInt(span12.innerText)) ? 
 if (parseInt(resultat.innerText) - 10 < 0) {
     paragraphe.innerHTML = 'le résultat est inférieur à 0';
 }
+
+container.style.backgroundColor = 'green'
